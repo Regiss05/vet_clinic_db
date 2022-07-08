ea6e04368a91dda5e752d37b008aed22aa0c7555
Regiss Mukubiza <regiss.steven@gmail.com>
2022-07-08 12:05:56 +0200

    average of animals born between 1990 and 2000

--- queries.sql
+++ queries.sql
@@ -29,3 +29,4 @@ SELECT * FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM 
 -- What is the minimum and maximum weight of each type of animal?
 SELECT MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG (weight_kg) FROM animals WHERE date_of_birth BETWEEN '1/1/1990' AND '1/1/2000';
